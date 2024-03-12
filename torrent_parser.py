35f9d9b851816e19a2a5024347ef39900e91fc75
jankalucki <jan.kalucki@outlook.com>
2024-03-12 12:20:04 +0100

    Bdecoding torrent file added

--- torrent_parser.py
+++ torrent_parser.py
@@ -1,12 +1,23 @@
 import aiofiles
+from bcoding import bdecode
 import asyncio
-async def read_torrent(filename:str):
-    async with aiofiles.open(file=filename,mode='r') as file :
+class torrent:
+    def __init__(self,torrent_dict) -> None:
         try:
-            content:str = await file.read()
-        except IndexError:
-            pass
-        await print(content)
+            self.length:int = int(torrent_dict['info']['length'])
+        except ValueError:
+            print("Invalid length value!")
+            return
+async def read_torrent(filename:str)->list[dict,bytes]:
+    async with aiofiles.open(file=filename,mode='br') as file :
+        content:str = await file.read()
+        decoded = bdecode(content)
+        pieces:bytes = decoded['info']['pieces']
+        print(type(pieces))
+        del decoded['info']
+        data = [decoded,pieces]
+        print(decoded)
+        return data
 if __name__ == "__main__":
     filename = "archlinux-2024.03.01-x86_64.iso.torrent"
     asyncio.run(read_torrent(filename))
