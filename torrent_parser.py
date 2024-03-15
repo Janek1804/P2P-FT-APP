6c345d3f8836b2d2e4bced0627314ee61c1b3cce
jankalucki <jan.kalucki@outlook.com>
2024-03-15 13:28:24 +0100

    finished torrent object constructor

--- torrent_parser.py
+++ torrent_parser.py
@@ -8,13 +8,20 @@ class torrent:
         except ValueError:
             print("Invalid length value!")
             return
+        self.name:str = torrent_dict['info']['name']
+        try:
+            self.piece_length:int = int(torrent_dict['info']['piece length'])
+        except ValueError:
+            print("Invalid piece length!")
+            return
+        self.url_list:list[str] =  torrent_dict['info']['url-list']
 async def read_torrent(filename:str)->list[dict,bytes]:
     async with aiofiles.open(file=filename,mode='br') as file :
         content:str = await file.read()
         decoded = bdecode(content)
         pieces:bytes = decoded['info']['pieces']
         print(type(pieces))
-        del decoded['info']
+        del decoded['info']['pieces']
         data = [decoded,pieces]
         print(decoded)
         return data
