e340497ec8a6b3f65eaadc1fd634f67bf7cf46a2
jankalucki <jan.kalucki@outlook.com>
2024-03-15 14:19:58 +0100

    peer exchange message defined & config file created

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ P2P Application for sharing files in LAN environment.
 ## Peer Exchange Protocol
 Peer advertisement- wanted resources- owned resources
     -fragment name-address and port
+PEX messages are exchanged on port 6771.
+
+## File Sharing
+shared_files.conf contains path to shared directory
+
+
--- peer-exchange.py
+++ peer-exchange.py
@@ -1,4 +1,17 @@
 from asyncio import open_connection
+from scapy.all import Packet
+from scapy.fields import *
+from scapy.sendrecv import AsyncSniffer
+async def listenPEX()-> None:
+    sniffer = AsyncSniffer(filter="udp dst port 6771")
+    sniffer.start()
 
 async def advertise(resources:list,bcast:str) -> None: 
     reader, writer = await open_connection(bcast,port=6771)
+
+class PEX(Packet):
+    name ="Peer Exchange"
+    fields_desc= [IPField(name="peer_address",default="127.0.0.1"),
+    IntField(name="port",default=6771),
+    StrField(name="Resources")
+    ]
