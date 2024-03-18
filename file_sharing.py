03014c517d95d6e12ba03c5b468120ce6aa16b96
jankalucki <jan.kalucki@outlook.com>
2024-03-18 15:23:06 +0100

    added piece creation and storing functions

--- file_sharing.py
+++ file_sharing.py
@@ -1,5 +1,13 @@
 import aiofiles
-from os import listdir
+from os import scandir
+async def shdir(path:str='shared',num_pieces=512,target='pieces') -> None:
+    tmp = scandir(path)
+    for filename in tmp:
+        if filename.is_file():
+            pieces:list[bytes]= await create_pieces(f'{path}/{filename}',num_pieces)
+            await store_pieces(pieces,f'{target}/{filename}')
+        elif filename.is_dir():
+            shdir(f'{path}/{filename}',num_pieces,target)
 
 async def create_pieces(filepath:str,num_pieces:int)->list[bytes]:
     async with aiofiles.open(filepath,'rb') as file:
@@ -12,3 +20,5 @@ async def store_pieces(pieces:list[bytes],path:str)->None:
         async with aiofiles.open(path,'ab') as file:
             for piece in pieces:
                 await file.write(piece)
+    else:
+        print("No pieces to write")
