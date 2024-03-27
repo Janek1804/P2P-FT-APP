1f6113f6efd774bd864547940100412b0a009fbb
noname135E <michal.jankowski800@gmail.com>
2024-03-27 15:23:23 +0100

    Fixed TCP server

--- peer_exchange.py
+++ peer_exchange.py
@@ -104,7 +104,7 @@ async def listenTCP(sock: Optional[socket.socket] = None, port: int = 7050, list
         - listen_addr (string) [default: ""] - IP address of the interface to listen at
         RETURNS NOTHING"""
     try:
-        if socket is None:
+        if sock is None:
             serv = await asyncio.start_server(handleRequests, listen_addr, port)
         else:
             serv = await asyncio.start_server(handleRequests, sock=sock)
@@ -170,16 +170,12 @@ async def obtainFromPeer(resource: str, peer: str, port: int = 7050) -> bytes:
         reader, writer = await open_connection(peer, port=port)
     except CancelledError:
         return piece
-    print("wwwwww")
     try:
-        print("conn")
         writer.write(f"REQUEST:{resource}".encode())
-        print("sent req")
         await writer.drain()
         try:
             data = await wait_for(reader.read(-1), timeout = 60)
             data = data.decode()
-            print(data)
             if data.startswith("CONTENT:"):
                 piece = data.partition(";")[2].encode()
         except TimeoutError:
