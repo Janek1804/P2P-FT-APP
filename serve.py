b18ce6f3dd4d364ec4472a5e31c362cbc6dc413d
jankalucki <jan.kalucki@outlook.com>
2024-03-28 14:18:28 +0100

    http server added

--- serve.py
+++ serve.py
@@ -1,16 +1,24 @@
 from aiohttp import web
 from globals import resource_list
+from asyncio import CancelledError
 
 routes = web.RouteTableDef()
 
-@routes.get('/')
+
 async def get_all(request):
-    return resource_list
+    return web.json_response(resource_list)
+
+async def get_file(request):
+    file_list = []
+    for res in resource_list:
+        if res.find(request.match_info['name']) != -1:
+            file_list.append(res)
+    return web.json_response(file_list)
 
 async def init():
     app = web.Application()
     runner = web.AppRunner(app)
-    app.router.add_get('/', get_all)
+    app.add_routes([web.get('/', get_all),web.get('/{name}',get_file)])
     await runner.setup()
     try:
         site = web.TCPSite(runner,'localhost',8080)
